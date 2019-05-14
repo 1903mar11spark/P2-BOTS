5b9a1cce1f543c52728c6624b259114213b7de3f
Em <ebaillie27@gmail.com>
2019-05-14 17:07:06 -0400

    Message
    message

--- AngularP2-May13/src/app/components/chart/chart.component.ts
+++ AngularP2-May13/src/app/components/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var CanvasJS: any;
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
-  }
-
+		let chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		exportEnabled: true,
+		title: {
+			text: "Angular"
+		},
+		data: [{
+			type: "column",
+			dataPoints: [
+				{ y: 71, label: "Apple" },
+				{ y: 55, label: "Mango" },
+				{ y: 50, label: "Orange" },
+				{ y: 65, label: "Banana" },
+				{ y: 95, label: "Pineapple" },
+				{ y: 68, label: "Pears" },
+				{ y: 28, label: "Grapes" },
+				{ y: 34, label: "Lychee" },
+				{ y: 14, label: "Jackfruit" }
+			]
+		}]
+	});
+		
+	chart.render();
+    }
 }
