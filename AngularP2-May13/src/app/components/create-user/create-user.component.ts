4d06bdf12c14e44067a578df4acbe6316edd3c78
Em <ebaillie27@gmail.com>
2019-05-14 23:21:54 -0400

    CAN ADDDDD A USER
    Waahahhahahahhahaha the monster has been created!!!

--- AngularP2-May13/src/app/components/create-user/create-user.component.ts
+++ AngularP2-May13/src/app/components/create-user/create-user.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model';
+import { UserType } from 'src/app/models/user-type.model';
+import { LoginCreds } from 'src/app/models/login-creds.model';
 
 
 
@@ -10,21 +13,41 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent implements OnInit {
-   
-  createForm = new FormGroup({
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    email: new FormControl()
-   }); 
+  
+  //instance variables because we have to lay this out 
+  createForm: FormGroup;
+  cUser: Object = {}; //still ??
+  user: User; 
+  uType: UserType;
+ 
+  constructor(private userService: UserService) { 
+    this.createForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      uName: new FormControl(),
+      pWord: new FormControl()
+     }); 
+  }
 
-   
+ 
    onFormSubmit(): void {
-     this.userService.addUser(this.createForm.value);
-    console.log(this.createForm.value);
+    let uType = new UserType(1, 'learner');
+
+    let creds = new LoginCreds(0, this.createForm.value.uName, this.createForm.value.pWord);
+    
+     let myUser = new User(0, this.createForm.value.firstName, this.createForm.value.lastName, this.createForm.value.email, uType, creds, 125);
+    
+     console.log(myUser);
+
+     this.userService.addUser(myUser).subscribe((creatingUser: any) => { this.user = creatingUser; console.log(this.user); },
+     error => { console.log(error); }
+    );
+    console.log(this.user);
+    
+  
 } 
-  constructor(private userService: UserService) { }
-
-
+  
 
   ngOnInit() {
   }
